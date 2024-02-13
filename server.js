ce81b2a9b648c1d5e2ed4612c07a6e80bc030a64
evan <guarneri1682@gmail.com>
2024-02-13 01:02:12 -0800

    02_home

--- server.js
+++ server.js
@@ -2,19 +2,41 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
-
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 running");
-});
+app.set("view engine", "ejs");
 
 app.get("/", (요청, 응답) => {
   응답.sendFile(__dirname + "/index.html");
 });
 
 app.get("/news", (요청, 응답) => {
+  db.collection("post").insertOne({ title: "blabla" });
   응답.send("rains a lot");
+});
+
+app.get("/list", async (요청, 응답) => {
+  let result = await db.collection("post").find().toArray();
+  console.log(result[0]);
+  응답.render("list.ejs", { posts: result });
 });
 
 app.get("/about", (요청, 응답) => {
   응답.sendFile(__dirname + "/about.html");
 });
+
+const { MongoClient } = require("mongodb");
+
+let db;
+const url =
+  "mongodb+srv://guarneri1682:09201120@dbmong.0wqva6p.mongodb.net/?retryWrites=true&w=majority";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum");
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 running");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
